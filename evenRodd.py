0edf2eb3697d16ab938b94310bbaa17c0da51db6
fayas0711 <47440783+fayas0711@users.noreply.github.com>
2019-02-18 14:04:45 +0530

    Update evenRodd.py

--- evenRodd.py
+++ evenRodd.py
@@ -1,5 +1,7 @@
-num = int(input())
-if (num % 2) == 0:
-   print("Even")
-else:
-   print("Odd")
+s=int(raw_input())
+if(s<0):
+    print("Invalid")
+elif(s%2==1):
+    print("Odd")
+elif(s%2==0):
+    print("Even")
